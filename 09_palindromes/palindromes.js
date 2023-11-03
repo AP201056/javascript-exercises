0e35304db1cf77f09aa8979dd68fb42c0f4e8496
peteralexa <peter.alexa@hotmail.com>
2023-11-03 12:47:19 +0100

    Solve exercise 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let stringToLowerCase = string.replace(/[^\w]|_/g, "").replace(/\s/g, "").toLowerCase();
+    let splitString = stringToLowerCase.split("");
+    let reverseString = splitString.reverse();
+    let rejoinString = reverseString.join("");
+    console.log(rejoinString)
+    return stringToLowerCase === rejoinString;
 };
+
+console.log(palindromes('A car, a man, a maraca.'));
 
 // Do not edit below this line
 module.exports = palindromes;

32388c251bc48a04bb5a3782b3e6fe2d64d0953b
peteralexa <peter.alexa@hotmail.com>
2023-11-03 13:56:58 +0100

    Solve exercise 10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num === 0) {
+        return 0
+    }
 
+    if (num < 0) {
+        return "OOPS"
+    }
+
+    let firstNum = 1
+    let secondNum = 0
+
+    for (let i=1; i <= num; i++) {
+        let resultNum = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = resultNum;
+    }
+
+    return secondNum
 };
+
+console.log(fibonacci(4))
 
 // Do not edit below this line
 module.exports = fibonacci;

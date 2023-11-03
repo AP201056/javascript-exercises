677afa496bd5f35c00e166eb563a1580958617e0
peteralexa <peter.alexa@hotmail.com>
2023-11-03 15:54:06 +0100

    Solve exercise 12-findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+   return array.reduce((oldest,current) => {
+        const ageOldest = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const ageCurrent = getAge(current.yearOfBirth, current.yearOfDeath);
+        return ageOldest < ageCurrent ? current : oldest
+    });
+    
+}
 
+function getAge(born,died) {
+    let age;
+    if (!died) {
+        age = (new Date().getFullYear()) - born;
+    } else {
+        age = died - born;
+        
+    }
+
+    return age;
 };
 
 // Do not edit below this line
